e23b377ebeb3d96bd5e558a3288379f1e17861d7
Izza <mathias.guiguen@gmail.com>
2020-08-13 10:55:13 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenue sur l'application web de Dionysos. 
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div className="memory"></div>
+  }
 }
+
+
+
 
 export default App;
