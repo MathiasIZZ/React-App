11eb4cf87ce32d1520f77d8e138c83bfa67656f5
Izza <mathias.guiguen@gmail.com>
2020-08-23 22:11:59 +0200

    ok

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import './Card.css'
 
 const HIDDEN_SYMBOL = '❓'
 
-const Card = () => <div className="card" />
+const Card = ({ card, feedback, onClick }) => (
+    <div className = { `card $ {feedback} ` } onClick={ () => onClick(card) }>
+        <span className="symbol">
+            { feedback === 'hidden' ? HIDDEN_SYMBOL : card}
+        </span>
+    </div>
+)
 
-export default Card
+Card.propTypes = {
+    card: PropTypes.string.isRequired, 
+    feedback: PropTypes.oneOf([
+        'visible',
+        'hiden',
+        'justMatched', 
+        'justeMisMatched',
+    ]).isRequired,
+    onClick: PropTypes.func.isRequired,
+
+
+}
+
+export default Card 
